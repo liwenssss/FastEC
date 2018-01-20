fe7752445d5d9082c55ce06cc4eedbf45416b3f7
CleverLove8 <573014453@qq.com>
2018-01-20 22:05:14 +0800

    init

---
+++ latte-core/src/main/java/mall/online/com/latte/app/ConfigType.java
@@ -0,0 +1,12 @@
+package mall.online.com.latte.app;
+
+/**
+ * Created by liWensheng on 2018/1/20.
+ */
+
+public enum  ConfigType {
+    API_HOST,               //网络请求域名
+    APPLICATION_CONTEXT,    //全局上下文
+    CONFIG_READY,           //控制初始化及配置的完成
+    ICON                    //存取个人初始化
+}
---
+++ latte-core/src/main/java/mall/online/com/latte/app/Latte.java
@@ -0,0 +1,20 @@
+package mall.online.com.latte.app;
+
+import android.content.Context;
+
+import java.util.WeakHashMap;
+
+/**
+ * Created by liWensheng on 2018/1/20.
+ */
+
+public final class Latte {
+    public static Configurator init(Context context) {
+        getConfigurations().put(ConfigType.APPLICATION_CONTEXT.name(), context.getApplicationContext());
+        return Configurator.getInstance();
+    }
+
+    private static WeakHashMap<String, Object> getConfigurations() {
+        return Configurator.getInstance().getFunctionConfigs();
+    }
+}
