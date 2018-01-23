8755bf6ff632a4ce7e1042e1081c0693cd996291
CleverLove8 <573014453@qq.com>
2018-01-23 10:17:44 +0800

    init retrofit service

---
+++ latte-core/src/main/java/mall/online/com/latte/net/HttpMethod.java
@@ -0,0 +1,15 @@
+package mall.online.com.latte.net;
+
+/**
+ * Created by liWensheng on 2018/1/23.
+ */
+
+public enum HttpMethod {
+    GET,
+    POST,
+    POST_RAW,
+    PUT,
+    PUT_RAW,
+    DELETE,
+    UOLOAD
+}
---
+++ latte-core/src/main/java/mall/online/com/latte/net/RestCreator.java
@@ -0,0 +1,57 @@
+package mall.online.com.latte.net;
+
+import java.util.concurrent.TimeUnit;
+
+import mall.online.com.latte.app.ConfigType;
+import mall.online.com.latte.app.Latte;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+
+/**
+ * Created by liWensheng on 2018/1/23.
+ * 创建retrofit
+ */
+
+public class RestCreator {
+
+    /**
+     * 创造方法，提供rest服务
+     * @return
+     */
+    public static RestService getRestService() {
+        return RestServiceHolder.REST_SERVICE;
+    }
+
+    /**
+     * 初始化retrofit实例
+     */
+    private static final class RetrofitHolder {
+        private static final String BASE_URL = (String)Latte.getConfigurations().get(ConfigType.API_HOST);
+        private static final Retrofit RETROFIT_CLIENT = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(OKHttpHolder.OK_HTTP_CLIENT)
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .build();
+    }
+
+    /**
+     * 初始化OkHttpClient
+     */
+    private static final class OKHttpHolder {
+        private static final int TIME_OUT = 60;
+
+        private static final OkHttpClient OK_HTTP_CLIENT =
+                new OkHttpClient.Builder()
+                .connectTimeout(TIME_OUT, TimeUnit.SECONDS)
+                .build();
+    }
+
+    /**
+     * 初始化rest服务
+     */
+    private static final class RestServiceHolder{
+        private static final RestService REST_SERVICE =
+                RetrofitHolder.RETROFIT_CLIENT.create(RestService.class);
+    }
+}
